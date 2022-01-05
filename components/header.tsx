e6fecf96c292b86797b302788675f33963a85052
noname2048 <sungwook.csw@gmail.com>
2022-01-05 21:00:57 +0900

    styles: change header-style

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,37 @@
 import Link from "next/link";
+import { Router } from "next/router";
 import { BiSearch } from "react-icons/bi";
+import { useRouter } from "next/router";
+
+// TODO: h1을 width에 따라 잘리게 되면 BC로 교체
+// TODO: 선택된 Link만 특정 style 적용
 
 export default function Header() {
+  const router = useRouter();
+  console.log(router);
   return (
-    <header className="flex flex-row justify-between items-end h-14 m-5">
-      <h1 className="basis-1/4 self-start bg-sky-400">BookCollecter</h1>
-      <nav className="basis-1/2 flex justify-around bg-green-400">
-        <Link href="/index">
-          <a>Home</a>
+    <header className="grid gap-4 grid-cols-3 grid-rows-1 justify-between items-center h-14 m-5 border-4 border-slate-700">
+      <div className="border-3 border-slate-700 flex justify-start">
+        <h1 className="text-3xl font-bold m-3">BookCollector</h1>
+      </div>
+      <nav className="flex self-end justify-around items-end">
+        <Link href="/">
+          <a className={router.asPath === "/" ? "underline" : ""}>Home</a>
         </Link>
         <Link href="/search">
-          <a>Detail Search</a>
+          <a className={router.asPath === "/search" ? "underline" : ""}>
+            Detail Search
+          </a>
         </Link>
         <Link href="/requests">
-          <a>Recent Requests</a>
+          <a className={router.asPath === "/requests" ? "underline" : ""}>
+            Recent Requests
+          </a>
         </Link>
       </nav>
-      <div className="basis-1/4 self-start flex flex-row bg-gray-400">
-        <input type="text" />
-        <BiSearch />
+      <div className="flex flex-row justify-end items-center">
+        <input type="text" className="border-2 border-slate-700 w-22" />
+        <BiSearch className="m-2" />
       </div>
     </header>
   );
