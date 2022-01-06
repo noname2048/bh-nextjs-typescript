68eae2758e6a5a07e14d1a05eab52a2badc40ca0
noname2048 <sungwook.csw@gmail.com>
2022-01-07 01:49:39 +0900

    feat:  add feature - push router (isbn13 search)

--- components/banner.tsx
+++ components/banner.tsx
@@ -9,37 +9,59 @@ const Banner: NextPage = () => {
   const [isbn13, setIsbn13] = useState("");
   return (
     <div>
-      <span>
-        사용자의 검색데이터를 통해 책을 모으는 DB 콜렉터 서비스 BookCollecter
-        입니다
-      </span>
-      <div>
-        <span>ISBN13으로 검색하기</span>
-        <input type="text" />
-        <BiSearch />
+      <div className="text-5xl m-10">
+        사용자의 검색데이터를 통해
+        <br />
+        책을 모으는 DB 콜렉터 서비스
+        <br />
+        BookCollecter 입니다
       </div>
-      <form
-        action="/search"
-        method="GET"
-        onSubmit={(e) => {
-          e.preventDefault();
-          router.push(`/search?title=${title}`);
-        }}
-      >
-        <span>제목으로 검색하기</span>
-        <input
-          name="title"
-          type="text"
-          onChange={(e) => {
-            setTitle(e.target.value);
+      <div className="flex flex-row justify-around m-10">
+        <form
+          className="flex flex-col border-2 p-20"
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(event.target?.title.value);
+            const isbn13 = event.target?.title.value || "";
+            router.push(`/search?isbn13=${isbn13}`);
           }}
-        />
-        <BiSearch
-          onClick={() => {
+        >
+          <span className="text-5xl text-center">ISBN13으로 검색하기</span>
+          <div className="flex flex-row justify-center items-center gap-1 my-2 border-2 rounded px-2 focus-within:border-sky-400 ">
+            <BiSearch className="text-2xl text-center" />
+            <input
+              className="h-10 w-full text-2xl text-center focus:border-transparent focus:outline-none"
+              name="title"
+              type="text"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+        <form
+          className="flex flex-col border-2 p-20"
+          action="/search"
+          method="GET"
+          onSubmit={(e) => {
+            e.preventDefault();
             router.push(`/search?title=${title}`);
           }}
-        />
-      </form>
+        >
+          <span className="text-5xl text-center">제목으로 검색하기</span>
+          <div className="flex flex-row justify-center items-center gap-1 my-2 border-2 rounded px-2 focus-within:border-sky-400 ">
+            <BiSearch className="text-2xl text-center" />
+            <input
+              className="h-10 w-full text-2xl text-center focus:border-transparent focus:outline-none"
+              name="title"
+              type="text"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
