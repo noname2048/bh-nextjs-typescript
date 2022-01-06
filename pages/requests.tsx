40a51482b0a380176075677a3a6d418964e4a248
noname2048 <sungwook.csw@gmail.com>
2022-01-06 23:04:33 +0900

    feat: add pages

---
+++ pages/requests.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from "next";
+
+const ReqeustList: NextPage = () => {
+  return <div></div>;
+};
---
+++ pages/search.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next/types";
+import { useState } from "react";
+
+const SearchResultList: NextPage = () => {
+  const [result, setResult] = useState([]);
+  return (
+    <>
+      {result.length === 0
+        ? "검색결과가 없습니다"
+        : result.map((item, idx) => <div key={idx}>{item}</div>)}
+    </>
+  );
+};
+
+export default SearchResultList;
