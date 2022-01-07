bc0b16de6e0b1c733bdac173dae683bc6382b581
noname2048 <sungwook.csw@gmail.com>
2022-01-07 20:46:07 +0900

    feat: shows feedback when backend not working

--- pages/requests.tsx
+++ pages/requests.tsx
@@ -2,11 +2,11 @@ import { NextPage } from "next";
 import { useState, useEffect } from "react";
 import useSWR from "swr";
 
-const whatUrl = "http://localhost:8000/what";
 const requestUrl = "http://localhost:8000/api/v1/books/requests";
 
 const RequestList: NextPage = () => {
-  const [result, setResult] = useState<Array<Object>>([]);
+  const [result, setResult] = useState([]);
+  const [serverError, setServerError] = useState(false);
 
   // const { data, error } = useSWR(
   //   "http://localhost:8000/api/v1/books/requests",
@@ -16,18 +16,31 @@ const RequestList: NextPage = () => {
   // if (error) return <div>에러가 발생했습니다</div>;
   // setResult(data);
 
+  // const fd = async (event) => {
+  //   event.preventDefault();
+  // }
+
   useEffect(() => {
     async function fetchData() {
-      const jsonData = await (await fetch(requestUrl)).json();
-      setResult(jsonData);
-      console.log(jsonData);
+      try {
+        const jsonData = await (await fetch(requestUrl)).json();
+        console.log(jsonData);
+        setResult(jsonData);
+        console.log(jsonData);
+        setServerError(false);
+      } catch (err) {
+        setResult([]);
+        setServerError(true);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <div>
-      {result.length === 0
+      {serverError
+        ? "서버에 오류가 있습니다."
+        : result.length === 0
         ? "검색결과가 없습니다"
         : result.map((item: Object, idx: number) => (
             <div key={idx}>{item}</div>
