6967c9cbf0674381454f9ff6cc1b65859a6d9078
noname2048 <sungwook.csw@gmail.com>
2022-01-08 23:49:35 +0900

    feat: confirm of no request exists

--- components/noResultAskAndSendRequestIsbn.tsx
+++ components/noResultAskAndSendRequestIsbn.tsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useSWR from "swr";
 
 export default function NoResultAskAndSendRequestIsbn({
   isbn13,
 }: {
   isbn13: number;
 }) {
-  // request에 있는지 확인하고
-  // request에 없으면 요청을 보낼것인지에 작성
-  return <div>HI</div>;
+  // request에 있는지 확인하고 요청보내기
+
+  const fetcher = (url: string, ...args: any[]) =>
+    fetch(url, ...args).then((res) => res.json());
+
+  const { data, error } = useSWR(
+    `/api/v1/books/requests/search?isbn13=${isbn13}`,
+    fetcher
+  );
+
+  const postFetcher = (url: string) =>
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ isbn13: isbn13 }),
+    });
+
+  if (data.length === 0) {
+    const { rData, rError } = useSWR(`/api/v1/books/requests`, postFetcher);
+  }
+
+  return <div></div>;
 }
