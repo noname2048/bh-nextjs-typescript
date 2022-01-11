a6d38c9b27aa524ce201078f751259f690751eda
noname2048 <sungwook.csw@gmail.com>
2022-01-11 22:28:12 +0900

    feat: add banner to some types, error msg

--- components/banner.tsx
+++ components/banner.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 
 const Banner: NextPage = () => {
   const router = useRouter();
-  const [title, setTitle] = useState("");
-  const [isbn13, setIsbn13] = useState("");
+  const [title, setTitle] = useState<string>("");
+  const [isbn13, setIsbn13] = useState<string>("");
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
   return (
     <div>
       <div className="text-5xl m-10">
@@ -22,25 +24,37 @@ const Banner: NextPage = () => {
           className="flex flex-col border-2 p-20 hover:border-gray-600 focus-within:border-black"
           onSubmit={(event) => {
             event.preventDefault();
-            console.log(event.target?.isbn13.value);
-            const isbn13 = event.target?.isbn13.value || "";
-            router.push(`/search?isbn13=${isbn13}`);
+            console.log(isbn13);
+            if (/^(\d-*){12}\d$/g.test(isbn13)) {
+              setErrorMsg("");
+              router.push(`/search?isbn13=${isbn13}`);
+            } else {
+              setErrorMsg("* 유효하지 않은 ISBN13 형식입니다.");
+            }
           }}
         >
           <span className="text-5xl text-center">ISBN13으로 검색하기</span>
-          <div className="flex flex-row justify-center items-center gap-1 my-2 border-2 rounded px-2 focus-within:border-sky-400">
+          <div className="flex flex-row justify-center items-center gap-1 mt-2 border-2 rounded px-2 focus-within:border-sky-400">
             <BiSearch className="text-2xl text-center" />
             <input
               className="h-10 w-full text-2xl text-center focus:border-transparent focus:outline-none"
               name="isbn13"
               type="text"
               onChange={(e) => {
-                setTitle(e.target.value);
+                setIsbn13(e.target.value);
+                if (errorMsg !== "") setErrorMsg("");
               }}
             />
           </div>
+          {errorMsg === "" ? (
+            <div className="text-white">a</div>
+          ) : (
+            <div className="text-red-700 flex flex-start">
+              <div>{errorMsg}</div>
+            </div>
+          )}
           <Link href={`search?isbn13=9791188102051`} passHref>
-            <div className="flex flex-row justify-center items-center">
+            <div className="justify-self-end flex flex-row justify-center items-center">
               <div className="grow-0 border-2 px-2 py-1 rounded-md bg-slate-200 hover:bg-slate-100">
                 <span>979-11-88102-05-1</span> 으로 검색해보기
               </div>
