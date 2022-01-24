9283921db80d98222ac141e802ea37ebabeeedf2
noname2048 <sungwook.csw@gmail.com>
2022-01-24 23:21:47 +0900

    feat: add nextjs rewrite feature (api hide)

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   images: {
     domains: ["job-book-image.s3.ap-northeast-2.amazonaws.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/books",
+        destination: `https://localhost:8000/api/v1/books`,
+      },
+    ];
+  },
 };
