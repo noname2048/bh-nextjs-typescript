cc9dbcab5322cd7b706cdacf0d81ed8adef22ac7
noname2048 <sungwook.csw@gmail.com>
2022-01-27 01:37:53 +0900

    feat: interval logic improves but not cleared

--- components/special/requestEffect.tsx
+++ components/special/requestEffect.tsx
@@ -3,66 +3,133 @@ import React, { useEffect, useState, useRef } from "react";
 import RequestModel from "../../interfaces/requestModel";
 import ResponseModel from "../../interfaces/responseModel";
 
+/**
+ * 1. request => 리스트로된 json data, RequestModel[] 타입의 데이터를 얻는다.
+ * 2. 해당 리스트에서 response_id가 등록되지 않은 개체를 찾아 모은다
+ * 3. 1초마다 다시 request를 보낸다
+ * 4. response_id가 모두 등록될때까지 3번과정을 진행하여 업데이트한다.
+ */
 export default function RequestEffect(): React.ReactElement {
   const [requests, setRequests] = useState<any>({
     data: null,
-    isLoading: true,
     error: null,
   });
 
   useEffect(() => {
     (async () => {
       try {
-        const json = await (await fetch(`/api/books/requsets`)).json();
-        setRequests({ data: json, isLoading: null, error: null });
+        const json = await (
+          await fetch(`http://localhost:8000/api/v1/books/requests`)
+        ).json();
+        setRequests({ data: json, error: null });
       } catch (err) {
-        setRequests({ data: null, isLoading: null, error: err });
+        setRequests({ data: null, error: err });
       }
     })();
-  }, []);
 
-  useEffect(() => {
-    (async () => {
-      const intervalId: any = setInterval(async () => {
-        if (requests?.data) {
-          const rq: RequestModel[] = requests.data;
-          const refresh = rq.filter((item: RequestModel) => !item.response_id);
-          if (refresh) {
-            const param = refresh
-              .map((item: RequestModel) => `id=${item.response_id}`)
-              .join("&");
-            try {
-              const json = await (await fetch(`/api/books/requsets`)).json();
-              const newJson = requests.map((old: RequestModel) => {
-                let same = json.filter(
-                  (item: RequestModel) => old.response_id == item.response_id
-                );
-                if (same) {
-                  return same;
-                }
-                return old;
-              });
+    const intervalId: any = setInterval(async () => {
+      console.log("interval", intervalId);
+      if (requests?.error) {
+        clearInterval(intervalId);
+      } else if (requests?.data) {
+        const refresh = requests.data.filter(
+          (item: RequestModel) => !item.response_id
+        );
+        if (refresh) {
+          const param = refresh
+            .map((item: RequestModel) => `id=${item.response_id}`)
+            .join("&");
 
-              setRequests({
-                data: newJson,
-                isLoading: null,
-                error: null,
-              });
-            } catch (err) {
-              setRequests({ data: null, isLoading: null, error: err });
-            }
-          } else {
-            return clearInterval(intervalId);
+          let newFetchData: any;
+          try {
+            newFetchData = await (
+              await fetch(
+                `http://localhost:8000/api/v1/books/requests` + param
+                  ? `?${param}`
+                  : ""
+              )
+            ).json();
+          } catch (err) {
+            setRequests({ data: null, error: err });
+            clearInterval(intervalId);
           }
+
+          const newRequestsData = requests.map((oldItem: RequestModel) => {
+            let same = newFetchData.filter(
+              (item: RequestModel) => oldItem.response_id == item.response_id
+            );
+            if (same) {
+              return same;
+            }
+            return oldItem;
+          });
+
+          setRequests({
+            data: newRequestsData,
+            error: null,
+          });
+        } else {
+          clearInterval(intervalId);
         }
-      }, 1000);
-    })();
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
+
+  // useEffect(() => {
+  //   console.log("useeffect02");
+  //   (async () => {
+  //     const intervalId: any = setInterval(async () => {
+  //       console.log("interval");
+  //       console.log(requests);
+  //       if (requests?.data) {
+  //         const rq: RequestModel[] = requests.data;
+  //         const refresh = rq.filter((item: RequestModel) => !item.response_id);
+  //         if (refresh) {
+  //           const param = refresh
+  //             .map((item: RequestModel) => `id=${item.response_id}`)
+  //             .join("&");
+  //           try {
+  //             const json = await (
+  //               await fetch(
+  //                 `http://localhost:8000/api/v1/books/requests` + param
+  //                   ? `?${param}`
+  //                   : ""
+  //               )
+  //             ).json();
+  //             const newJson = requests.map((old: RequestModel) => {
+  //               let same = json.filter(
+  //                 (item: RequestModel) => old.response_id == item.response_id
+  //               );
+  //               if (same) {
+  //                 return same;
+  //               }
+  //               return old;
+  //             });
+
+  //             setRequests({
+  //               data: newJson,
+  //               isLoading: null,
+  //               error: null,
+  //             });
+  //           } catch (err) {
+  //             setRequests({ data: null, isLoading: null, error: err });
+  //           }
+  //         } else {
+  //           return clearInterval(intervalId);
+  //         }
+  //       }
+  //     }, 1000);
+  //   })();
+  // }, []);
 
   return (
     <>
-      {requests
-        ? requests?.data.map((item: ResponseModel) => item.isbn13)
+      {requests?.data
+        ? requests?.data.map((item: ResponseModel) => (
+            <div key={item.id}>{item.isbn13}</div>
+          ))
         : null}
     </>
   );
